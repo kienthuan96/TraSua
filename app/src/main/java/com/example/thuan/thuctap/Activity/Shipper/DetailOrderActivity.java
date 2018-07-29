58f18d800a6a7782de212fd0058a550d85588e24
triplethuan96 <triplethuan96@gmail.com>
2018-07-29 23:20:07 +0700

    shipper

---
+++ app/src/main/java/com/example/thuan/thuctap/Activity/Shipper/DetailOrderActivity.java
@@ -0,0 +1,13 @@
+package com.example.thuan.thuctap.Activity.Shipper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_order);
+    }
+}
