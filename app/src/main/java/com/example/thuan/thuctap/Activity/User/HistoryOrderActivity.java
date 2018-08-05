904e95a317b7d8c5d79ee2473d67b24f9a631fa4
triplethuan96 <triplethuan96@gmail.com>
2018-08-05 23:07:24 +0700

    rating

--- app/src/main/java/com/example/thuan/thuctap/Activity/User/HistoryOrderActivity.java
+++ app/src/main/java/com/example/thuan/thuctap/Activity/User/HistoryOrderActivity.java
@@ -1,12 +1,18 @@
 package com.example.thuan.thuctap.Activity.User;
 
+import android.app.Dialog;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RatingBar;
 import android.widget.Toast;
 
+import com.example.thuan.thuctap.Activity.Shipper.HistoryRegisterOrderActivity;
 import com.example.thuan.thuctap.Adapter.User.HistoryOrderAdapter;
 import com.example.thuan.thuctap.Model.Order;
 import com.example.thuan.thuctap.R;
@@ -17,6 +23,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -25,6 +32,11 @@ public class HistoryOrderActivity extends AppCompatActivity {
     private HistoryOrderAdapter adapter;
     private ListView lstHistoryOrder;
     private String idUser;
+    private Integer number;
+    private Integer rate;
+    private Dialog dialog;
+    private RatingBar rtbOrder;
+    private Button btnRating;
 
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
@@ -37,6 +49,7 @@ public class HistoryOrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_history_order);
         getId();
         getData();
+        event();
     }
 
     private void getId() {
@@ -82,6 +95,46 @@ public class HistoryOrderActivity extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
+            }
+        });
+    }
+
+    private void event() {
+        dialog = new Dialog(HistoryOrderActivity.this);
+        dialog.setContentView(R.layout.layout_rating);
+        dialog.setTitle("Đánh giá");
+
+        rtbOrder = dialog.findViewById(R.id.rtbOrder);
+        btnRating = dialog.findViewById(R.id.btnRating);
+
+        rtbOrder.setRating(3);
+        lstHistoryOrder.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (arrayList.get(i).getStatus().equals("Done")) {
+                    dialog.show();
+                    number = i;
+                }
+            }
+        });
+
+        btnRating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rate = Math.round(rtbOrder.getRating());
+                myRefOrder.child(arrayList.get(number).getId()).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Toast.makeText(HistoryOrderActivity.this, ""+rtbOrder.getRating(), Toast.LENGTH_SHORT).show();
+                        dataSnapshot.getRef().child("rate").setValue(rate);
+                        dialog.cancel();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
             }
         });
     }
